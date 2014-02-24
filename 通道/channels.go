389bc8e2fda7029abbf91db376f9905cd58ca58f
iloveyuedu <780860343@qq.com>
2014-02-24 16:32:02 +0800

    tab转空格

--- 通道/channels.go
+++ 通道/channels.go
@@ -7,17 +7,17 @@ import "fmt"
 
 func main() {
 
-	// 通过 `make(chan val-type)` 创建一个新的
-	// 是有类型的，指定传递的数据类型
+    // 通过 `make(chan val-type)` 创建一个新的
+    // 是有类型的，指定传递的数据类型
     messages := make(chan string)
 
     // 发送值到内使用 ` <-` 语法,这里我们发送
-	// `"ping"`到`messages`内,这个通道是我们上面
-	// 创建的，在一个新的协程里使用
+    // `"ping"`到`messages`内,这个通道是我们上面
+    // 创建的，在一个新的协程里使用
     go func() { messages <- "ping" }()
 
-	// `<-channel`语法 用来接收一个通道内的值，这里我们
-	// 接收到`"ping"`消息，我们刚刚在上面发送的消息，并把它打印出来
+    // `<-channel`语法 用来接收一个通道内的值，这里我们
+    // 接收到`"ping"`消息，我们刚刚在上面发送的消息，并把它打印出来
     msg := <-messages
     fmt.Println(msg)
 }
