eb769fe90c54f6ccc83d3331808cded43afc5ab4
iloveyuedu <780860343@qq.com>
2014-03-05 08:58:46 +0800

    ..

--- 命令行标记/command-line-flags.go
+++ 命令行标记/command-line-flags.go
@@ -11,28 +11,28 @@ import "fmt"
 func main() {
 
     // 可以定义标记为字符串，整形和布尔型，这里我们定义了一个
-	// 字符串标记 `word` 且其默认值为`"foo"`并且有一个短的描述
-	// `flag.String`函数返回一个字符串指针(不是一个字符串值)
-	// 下面我们将看到如何使用这个字符串指针
+    // 字符串标记 `word` 且其默认值为`"foo"`并且有一个短的描述
+    // `flag.String`函数返回一个字符串指针(不是一个字符串值)
+    // 下面我们将看到如何使用这个字符串指针
     wordPtr := flag.String("word", "foo", "a string")
 
     // 定义名为 `numb` 和 `fork` 的标记，使用和上面相似的
-	// 方法来获取其值
+    // 方法来获取其值
     numbPtr := flag.Int("numb", 42, "an int")
     boolPtr := flag.Bool("fork", false, "a bool")
 
     // 也可以使用一个存在的变量来作为标记定义的一部分
-	// 更方便的获取命令行标记的值，注意我们需要传递指针到
-	// StringVar函数里
+    // 更方便的获取命令行标记的值，注意我们需要传递指针到
+    // StringVar函数里
     var svar string
     flag.StringVar(&svar, "svar", "bar", "a string var")
 
     // 当所有的标记都定义完成之后，通过调用`flag.Parse()`
-	// 来完成命令行标记的解析
+    // 来完成命令行标记的解析
     flag.Parse()
 
     // 下面我们打印出所有解析之后的结果和命令行参数
-	// 注意我们需要对指针进行解引用`*wordPtr`来获取其指向的值
+    // 注意我们需要对指针进行解引用`*wordPtr`来获取其指向的值
     fmt.Println("word:", *wordPtr)
     fmt.Println("numb:", *numbPtr)
     fmt.Println("fork:", *boolPtr)
